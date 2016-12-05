5d298558f3ec401ac8cd4bd0f13bbd881b2c21c4
Mebin Skaria <mebins@gmail.com>
2016-12-04 20:22:33 -0800

    Created the Light Up and Light Off Function which causes the Buzzer to beep a certain frequency depending on the button that is pressed. Also Lights turn on and off depending on the button that is pressed.

--- MemoryGame.ino
+++ MemoryGame.ino
@@ -1,32 +1,69 @@
 //prototypes
 void lightUp(int LED);
-const int LEDS[] = {3,4,5};
-const int PUSHBUTTONS[] = {A0,A1,A2};
-const int BUZZER = A3;
-const int SOUNDFREQUENCIES[] = {90,140,180};
-enum GAMESTATE{LOST, START, CONTINUE};
+void lightOff(int LED);
+
+//Declerations
+const int LEDS[] = {13, 12, 11};
+const int PUSHBUTTONS[] = {A0, A1, A2};
+int buttonStates[3] = {0};
+const int BUZZER = 10;
+const int SOUNDFREQUENCIES[] = {200, 240, 255};
+enum GAMESTATE {LOST, START, CONTINUE};
+GAMESTATE gamestate;
+boolean pressed;
 
 void setup() {
-  GAMESTATE gamestate = START;
+  pressed = true;
+  gamestate = START;
   // put your setup code here, to run once:
-  for(int i = 0; i < 3; i++)
+  for (int i = 0; i < 3; i++)
   {
-    pinMode(LEDS[i],OUTPUT);
-    pinMode(PUSHBUTTONS[i],INPUT);
+    pinMode(LEDS[i], OUTPUT);
+    pinMode(PUSHBUTTONS[i], INPUT);
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(gamestate == START)
+  if (gamestate == START)
   {
-    
+    lightUp(0);
+    delay(500);
+    lightUp(1);
+    delay(500);
+    lightUp(2);
+    delay(500);
+    lightOff(2);
+    gamestate = CONTINUE;
   }
-  else if(gamestate == CONTINUE)
+  else if (gamestate == CONTINUE)
   {
-    
+    while ((buttonStates[0] = digitalRead(PUSHBUTTONS[0])) == LOW && (buttonStates[1] = digitalRead(PUSHBUTTONS[1])) == LOW && (buttonStates[2] = digitalRead(PUSHBUTTONS[2])) == LOW)
+    {
+      pressed = false;
+    }
+    if (!pressed)
+    {
+      if (buttonStates[0]) {
+        lightUp(0);
+        pressed = true;
+      }
+      else if (buttonStates[1]) {
+        lightUp(1);
+        pressed = true;
+      }
+      else if (buttonStates[2]) {
+        lightUp(2);
+        pressed = true;
+      }
+      delay(250);
+      for (int i = 0; i < 3; i++)
+      {
+        lightOff(i);
+      }
+    }
   }
-  else if(gamestate == LOST)
+  else if (gamestate == LOST)
   {
 
   }
@@ -34,17 +71,21 @@ void loop() {
 
 void lightUp(int LED)
 {
-  for(int i = 0; i < 3; i++)
+  for (int i = 0; i < 3; i++)
   {
-    if(i != LED)
+    if (i != LED)
     {
-      digitalWrite(LEDS[i],LOW);
+      digitalWrite(LEDS[i], LOW);
     }
     else
     {
-      digitalWrite(LEDS[i],HIGH);
-      analogWrite(BUZZER,SOUNDFREQUENCIES[i]);
+      digitalWrite(LEDS[i], HIGH);
+      tone(BUZZER, SOUNDFREQUENCIES[i], 250);
     }
   }
+}
+void lightOff(int LED)
+{
+  digitalWrite(LEDS[LED], LOW);
 }
 
