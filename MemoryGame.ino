142427ea995debd6b93fc2b66840736b37507799
Mebin Skaria <mebins@gmail.com>
2016-12-04 21:38:35 -0800

    Implemented Main Game Logic, Pattern Holding Array, Pattern Randomizer, Pattern Seeder, Pattern and Input Checker, Level Up Counter, Restart, and Continue Finished

--- MemoryGame.ino
+++ MemoryGame.ino
@@ -1,19 +1,32 @@
 //prototypes
 void lightUp(int LED);
-void lightOff(int LED);
+boolean checker(int input, int index);
+void playPattern();
 
 //Declerations
+#define SIZE 100
 const int LEDS[] = {13, 12, 11};
 const int PUSHBUTTONS[] = {A0, A1, A2};
 int buttonStates[3] = {0};
 const int BUZZER = 10;
+const int NOISEPIN = A3;
 const int SOUNDFREQUENCIES[] = {200, 240, 255};
+
 enum GAMESTATE {LOST, START, CONTINUE};
 GAMESTATE gamestate;
+int level;
+
+
 boolean pressed;
 
+int pattern[SIZE];
+int index;
+
+
 void setup() {
-  pressed = true;
+  randomSeed(analogRead(NOISEPIN));
+  Serial.begin(9600);
+  pressed = false;
   gamestate = START;
   // put your setup code here, to run once:
   for (int i = 0; i < 3; i++)
@@ -27,45 +40,87 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (gamestate == START)
   {
+    for (int i = 0; i < SIZE; i++)
+    {
+      pattern[i] = (int)random(3);
+    }
+    
+    level = 0;
+    index = 0;
     lightUp(0);
     delay(500);
     lightUp(1);
     delay(500);
     lightUp(2);
     delay(500);
-    lightOff(2);
     gamestate = CONTINUE;
   }
   else if (gamestate == CONTINUE)
   {
-    while ((buttonStates[0] = digitalRead(PUSHBUTTONS[0])) == LOW && (buttonStates[1] = digitalRead(PUSHBUTTONS[1])) == LOW && (buttonStates[2] = digitalRead(PUSHBUTTONS[2])) == LOW)
+    //START OF LEVEL
+    levelUp();
+    playPattern();
+    Serial.print("Level: ");
+    Serial.print(level);
+    Serial.print(" Index: ");
+    Serial.println(index);
+    while (index <= level && gamestate == CONTINUE)
     {
-      pressed = false;
-    }
-    if (!pressed)
-    {
-      if (buttonStates[0]) {
-        lightUp(0);
-        pressed = true;
-      }
-      else if (buttonStates[1]) {
-        lightUp(1);
-        pressed = true;
-      }
-      else if (buttonStates[2]) {
-        lightUp(2);
-        pressed = true;
-      }
-      delay(250);
-      for (int i = 0; i < 3; i++)
+      while ((buttonStates[0] = digitalRead(PUSHBUTTONS[0])) == LOW && (buttonStates[1] = digitalRead(PUSHBUTTONS[1])) == LOW && (buttonStates[2] = digitalRead(PUSHBUTTONS[2])) == LOW)
       {
-        lightOff(i);
+        pressed = false;
       }
+      if (!pressed)
+      {
+        if (buttonStates[0]) {
+          pressed = true;
+          if (checker(0, index))
+          {
+            lightUp(0);
+            index++;
+          }
+          else
+          {
+            gamestate = LOST;
+          }
+        }
+        else if (buttonStates[1]) {
+          pressed = true;
+          if (checker(1, index))
+          {
+            lightUp(1);
+            index++;
+          }
+          else
+          {
+            gamestate = LOST;
+          }
+
+        }
+        else if (buttonStates[2]) {
+          pressed = true;
+          if (checker(2, index))
+          {
+            lightUp(2);
+            index++;
+          }
+          else
+          {
+            gamestate = LOST;
+          }
+
+        }
+      }
+    }
+    if (index > level)
+    {
+      level++;
+      index = 0;
     }
   }
   else if (gamestate == LOST)
   {
-
+      gamestate = START;
   }
 }
 
@@ -83,9 +138,41 @@ void lightUp(int LED)
       tone(BUZZER, SOUNDFREQUENCIES[i], 250);
     }
   }
+  delay(250);
+  for (int i = 0; i < 3; i++)
+  {
+    digitalWrite(LEDS[i], LOW);
+  }
+  delay(250);
 }
-void lightOff(int LED)
+
+void levelUp()
 {
-  digitalWrite(LEDS[LED], LOW);
+  for (int i = 0; i < 3; i++)
+  {
+    digitalWrite(LEDS[i], HIGH);
+  }
+  delay(250);
+  for (int i = 0; i < 3; i++)
+  {
+    digitalWrite(LEDS[i], LOW);
+  }
+  delay(250);
+}
+void playPattern()
+{
+  for (int i = 0; i <= level; i++)
+  {
+    lightUp(pattern[i]);
+  }
+}
+
+boolean checker(const int input, const int index)
+{
+  if (input == pattern[index])
+  {
+    return true;
+  }
+  return false;
 }
 
